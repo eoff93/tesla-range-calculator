09747fa61e5fd9dfd3d1be5f15ab6f79f9158fca
eoff93 <bane.dev101@gmail.com>
2017-01-01 12:33:57 +0100

    Setup tesla module and data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeslaBatteryModule
   ],
   imports: [
     BrowserModule,
---
+++ src/app/tesla-battery/containers/tesla-battery/tesla-battery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tesla-battery',
+  templateUrl: './tesla-battery.component.html',
+  styleUrls: ['./tesla-battery.component.less']
+})
+export class TeslaBatteryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
